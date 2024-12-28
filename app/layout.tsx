282d7006b35ba1fa0191f8695353a80c607be66b
Mayank <shnkr.mynk@gmail.com>
2024-12-28 01:02:14 +0100

    implementing vercel analytics v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,35 @@
-import type { Metadata } from 'next'
-import {  Montserrat  } from 'next/font/google'
-import './globals.css'
-import { Analytics } from '@vercel/analytics/next'
+  import { Analytics } from "@vercel/analytics/react"
+  import type { Metadata } from 'next'
+  import {  Montserrat  } from 'next/font/google'
+  import './globals.css'
 
-const font = Montserrat({ subsets: ['latin'], weight:['400','700']})
 
-export const metadata = {
-  title: 'CapIA',
-  description: 'Your fintech solution',
-  icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: 'any' },
-      { url: '/favicon.png', type: 'image/png', sizes: '192x192' },
-    ],
-    shortcut: '/favicon.ico',
-    apple: { url: '/apple-touch-icon.png', sizes: '192x192' },
-  },
-  manifest: '/manifest.json',
-};
+  const font = Montserrat({ subsets: ['latin'], weight:['400','700']})
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
-  )
-}
+  export const metadata = {
+    title: 'CapIA',
+    description: 'Your fintech solution',
+    icons: {
+      icon: [
+        { url: '/favicon.ico', sizes: 'any' },
+        { url: '/favicon.png', type: 'image/png', sizes: '192x192' },
+      ],
+      shortcut: '/favicon.ico',
+      apple: { url: '/apple-touch-icon.png', sizes: '192x192' },
+    },
+    manifest: '/manifest.json',
+  };
+
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body className={font.className}>{children}
+          <Analytics />
+        </body>
+      </html>
+    )
+  }
